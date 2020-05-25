a397336309104931b9a327c4777f8f06c4675c15
choho1515 <65482819+choho1515@users.noreply.github.com>
2020-05-25 15:39:15 +0900

    Add files via upload

---
+++ PCRoom.js
@@ -0,0 +1,15 @@
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if (msg.startsWith("!레식PC방 ")) {
+        sp = msg.substr(7).split(" ");
+        const count = 21;
+        let u = org.jsoup.Jsoup.connect("http://rainbow6pcbang.pmang.com/?mKey=2&sKey=1&ctrl=pcbang%3Apcbang_list&keyword=" + sp[0] + "&page=" + sp[1]).get()
+        var box = [];
+        for (i = 1; i < u.select(".pcbang").size() - 1; i++) {
+            var pc_name = u.select(".pcbang").get(i).text().toUpperCase();
+            var pc_tel = u.select(".tel").get(i).text()
+            var pc_add = u.select(".address").get(i).text()
+            box.push(i + ".\n??이름 : " + pc_name + "\n?? 전화번호 : " + pc_tel + "\n?? 주소 : " + pc_add + "\n")
+        }
+        replier.reply(box.join("\n").trim())
+    }
+}
